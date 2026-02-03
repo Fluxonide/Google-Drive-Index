3234edac72bf748fdffbfccf03888d7a87b3036a
rhvetican <ranitz.2005@gmail.com>
2026-02-03 19:44:06 +0530

    build: add version/date banner to JS & CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import packageJson from './package.json'
+
+const version = packageJson.version
+const buildDate = new Date().toISOString()
+const banner = `/*! Build: ${version} - ${buildDate} */`
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'css-banner',
+      apply: 'build',
+      generateBundle(options, bundle) {
+        for (const fileName in bundle) {
+          if (fileName.endsWith('.css')) {
+            const chunk = bundle[fileName]
+            if (chunk.type === 'asset' && typeof chunk.source === 'string') {
+              chunk.source = `${banner}\n${chunk.source}`
+            }
+          }
+        }
+      }
+    }
+  ],
   build: {
     outDir: 'public/build',
     emptyOutDir: true,
@@ -12,7 +33,8 @@ export default defineConfig({
       output: {
         entryFileNames: '[name].js',
         chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
+        assetFileNames: '[name].[ext]',
+        banner: banner
       }
     }
   }
